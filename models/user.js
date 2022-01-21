0b2fd1d17c3ea98a774b13bae8318baf250c5a7b
user.email <pavelchi91@gmail.com>
2022-01-21 16:17:56 +0200

    Working server framework example

---
+++ models/user.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var User = new Schema({
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
+    facebookId: String,
+    admin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+//automatically enables storing of username and password(also hashes password)
+User.plugin(passportLocalMongoose);
+
+
+//gets imported by authenticate.js
+module.exports = mongoose.model('User', User);
