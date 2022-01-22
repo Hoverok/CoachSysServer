0c82b0ca663cae41bbaeb53fa004d4496c2c875e
user.email <pavelchi91@gmail.com>
2022-01-22 15:59:42 +0200

    Added specialization booleans

--- models/adverts.js
+++ models/adverts.js
@@ -29,17 +29,62 @@ const achievmentSchema = new Schema({
     }
 });
 
+const specializationSchema = new Schema({
+    power: {
+        type: Boolean,
+    },
+    endurance: {
+        type: Boolean,
+    },
+    hiit: {
+        type: Boolean,
+    },
+    crossfit: {
+        type: Boolean,
+    },
+    rehab: {
+        type: Boolean,
+    },
+    weightLoss: {
+        type: Boolean,
+    },
+    weightGain: {
+        type: Boolean,
+    },
+    bodySculpt: {
+        type: Boolean,
+    },
+});
+
 const advertSchema = new Schema({
     description: {
         type: String,
         required: true
     },
-
-    specialization: {
-        type: String
-        // default: ''
+    power: {
+        type: Boolean,
     },
-
+    endurance: {
+        type: Boolean,
+    },
+    hiit: {
+        type: Boolean,
+    },
+    crossfit: {
+        type: Boolean,
+    },
+    rehab: {
+        type: Boolean,
+    },
+    weightLoss: {
+        type: Boolean,
+    },
+    weightGain: {
+        type: Boolean,
+    },
+    bodySculpt: {
+        type: Boolean,
+    },
     pastJobs: [pastJobSchema],
     achievements: [achievmentSchema]
 
@@ -56,18 +101,28 @@ var Adverts = mongoose.model('Advert', advertSchema);
 module.exports = Adverts;
 
 // {
-//     "description" : "Esu 2 treneris",
-//         "workExperience" : "2019-2020",
-//             "achievements" : "dafiga",
-//                 "specialization" : "kachalka",
-//                     "jobExperiences" : [
-
-//                     ]
+//     "description": "Esu 2 treneris",
+//     "workExperience": "2019-2020",
+//     "power": true,
+//     "endurance": false,
+//     "hiit" : true,
+//     "crossfit" : false,
+//     "rehab" : true,
+//     "weightLoss" : false,
+//     "weightGain" : true,
+//     "bodySculpt" : false,
+//     "jobExperiences": [],
+//     "achievements": []
 // }
 //PAST JOBS
-                        // {
-                        //     "startDate": "2020-01-01",
-                        //     "endDate": "2222222",
-                        //     "jobDescription": "Solid darbas"
-                        // }
+// {
+//     "startDate": "2020-01-01",
+//         "endDate": "2222222",
+//             "jobDescription": "Solid darbas"
+// }
 //ACHIEVEMENTS
+// {
+//     "achievementName": "Champion of the world",
+//     "achevementDescription": "laimėjau pasaulės krepšinio taurę",
+//     "achievementDate": "1914"
+// }
